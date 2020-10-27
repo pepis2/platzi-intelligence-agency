0bd8bde5ff976490714fbf3467515ca22b209d75
Pepis2 <pepitus.petaca@gmail.com>
2020-10-27 03:13:28 -0300

    cambio el archivo js para que llame al json propio

--- cia/fetchData.js
+++ cia/fetchData.js
@@ -3,7 +3,7 @@ let = $accordion = document.getElementById("accordion");
 let ciaData = null;
 
 fetch(
-  "https://app.scrapinghub.com/api/v2/datasets/kdybI1E79Ww/download?format=json"
+  "./cia.json"
 )
   .then((response) => {
     return response.json();
